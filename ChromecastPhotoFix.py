2968a7b72cf5d2a5d2df3c67d0fd4c0b412583a1
Eric Nachtigal <Eric.Nachtigal@gmail.com>
2023-07-31 16:07:22 -0400

    Iterates through selected directory

--- ChromecastPhotoFix.py
+++ ChromecastPhotoFix.py
@@ -1,36 +1,49 @@
 # Importing Image class from PIL module
 from PIL import Image, ImageFilter
- 
-# Rescales the original image for the center print
-imscale = Image.open(r"C:\Users\Eric\Pictures\ChromecastFix\Original\49214503798_03b510311d_k.jpg")
-width, height = imscale.size
-print("Original", imscale.size)
-scale = 1920, 1080
-imscale.thumbnail(scale, Image.Resampling.LANCZOS)
-width, height = imscale.size
-print("Rescale", imscale.size)
-RescaleWidth = imscale.size[0]
-# imscale.show()
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+import os
 
-# Crops the original image and blurs
-imcrop = Image.open(r"C:\Users\Eric\Pictures\ChromecastFix\Original\49214503798_03b510311d_k.jpg")
-width, height = imcrop.size
-cropscale = 1920, (1920/imcrop.size[0])*imcrop.size[1]
-imcrop.thumbnail(cropscale, Image.Resampling.LANCZOS)
-center = imcrop.size[1]/2
-top = center+540
-bottom = center-540
-cropbox = (0, bottom, 1920, top)
-crop = imcrop.crop(cropbox)
-print("Crop", imcrop.size)
-gaussImage = crop.filter(ImageFilter.GaussianBlur(20))
+path = askdirectory(title='Select Folder') # shows dialog box and return the path
+for x in os.listdir(path):
+    if x.endswith(".jpg"):
+        # Prints only JPEG files present in selected directory
+        print(path+x)
+        imscale = Image.open(os.path.join(path, x))
 
-# Pastes the scaled image over the blured image
-shift = int((1920-RescaleWidth)/2), 0
-print("Shift", shift)
-gaussImage.paste(imscale, shift)
-gaussImage.show()
+        # Rescales the original image for the center print
+        imscale = Image.open(os.path.join(path, x))
+        width, height = imscale.size
+        print("Original", imscale.size)
+        scale = 1920, 1080
+        imscale.thumbnail(scale, Image.Resampling.LANCZOS)
+        width, height = imscale.size
+        print("Rescale", imscale.size)
+        RescaleWidth = imscale.size[0]
+        # imscale.show()
 
+        # Crops the original image and blurs
+        imcrop = Image.open(os.path.join(path, x))
+        width, height = imcrop.size
+        cropscale = 1920, (1920/imcrop.size[0])*imcrop.size[1]
+        imcrop.thumbnail(cropscale, Image.Resampling.LANCZOS)
+        center = imcrop.size[1]/2
+        top = center+540
+        bottom = center-540
+        cropbox = (0, bottom, 1920, top)
+        crop = imcrop.crop(cropbox)
+        print("Crop", imcrop.size)
+        gaussImage = crop.filter(ImageFilter.GaussianBlur(20))
+
+        # Pastes the scaled image over the blured image
+        shift = int((1920-RescaleWidth)/2), 0
+        print("Shift", shift)
+        gaussImage.paste(imscale, shift)
+        gaussImage.show()
+        imscale.close()
+        imcrop.close()
+        gaussImage.close
+        
 # Sources Image Manipulation
 # https://pillow.readthedocs.io/en/stable/handbook/tutorial.html#using-the-image-class
 # https://www.geeksforgeeks.org/python-pil-image-crop-method/ 
@@ -40,3 +53,9 @@ gaussImage.show()
 # https://note.nkmk.me/en/python-pillow-paste/#:~:text=Call%20the%20paste()%20method,left)%20of%20the%20base%20image.
 # https://pillow.readthedocs.io/en/latest/reference/Image.html (Image.paste)
 # https://www.geeksforgeeks.org/how-to-convert-float-to-int-in-python/
+
+# Select images and directories.
+# https://stackoverflow.com/questions/50860640/ask-a-user-to-select-folder-to-read-the-files-in-python
+# 
+#
+#
